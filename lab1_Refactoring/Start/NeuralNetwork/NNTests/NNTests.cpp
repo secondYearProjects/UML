1eee31965186c860fd16cf31e99e2edadd15de85
SergySanJj <isara.isara8@gmail.com>
2019-04-14 18:07:04 +0300

    Remove GTests, replace with Native Tests

---
+++ lab1_Refactoring/Start/NeuralNetwork/NNTests/NNTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NNTests
+{
+	TEST_CLASS(NNTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
